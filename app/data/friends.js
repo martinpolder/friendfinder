7b12e3d49200bc0af6402822253ea1c6a6923109
Martin Polder <martinpolder@gmail.com>
2019-09-09 15:33:20 -0400

    added images/data started html added css file

---
+++ app/data/friends.js
@@ -0,0 +1,181 @@
+// This will hold the array containing the friends that users can be matched with
+
+var friendArr = [
+    //Friend one
+    {
+        "name": "Herbet Schubert",
+        "photo": "/photos/bill-farnsworth.jpg",
+        // 10 scores
+        "scores": [
+        5,
+        1,
+        4,
+        2,
+        3,
+        4,
+        3,
+        5,
+        1,
+        1, ]
+    
+},
+    // friend two
+    {
+        "name": "Leroy Crawford",
+        "photo": "/photos/leroy-crawford.jpg",
+        // 10 scores
+        "scores": [
+        2,
+        5,
+        4,
+        2,
+        3,
+        4,
+        5,
+        2,
+        4,
+        3, ]
+},
+    // friend three
+    {
+        "name": "Reginald Chevrolet",
+        "photo": "/photos/reginald-chevrolet.jpg",
+        // 10 scores
+        "scores": [
+        5,
+        3,
+        1,
+        4,
+        3,
+        1,
+        2,
+        5,
+        2,
+        4, ]
+},
+    // friend four
+    {
+        "name": "Clementine La'Equinox",
+        "photo": "/photos/clementinelaequinox.jpg",
+        // 10 scores
+        "scores": [
+        1,
+        3,
+        4,
+        4,
+        2,
+        5,
+        3,
+        1,
+        2,
+        2, ]
+},
+    // friend five
+    {
+        "name": "Henrietta Von Featherthorn",
+        "photo": "/photos/henrietta-featherthorn.jpg",
+        // 10 scores
+        "scores": [
+        5,
+        1,
+        2,
+        3,
+        1,
+        5,
+        2,
+        4,
+        3,
+        2, ]
+},
+    // friend six
+    {
+        "name": "Llewellyn Jacksonian",
+        "photo": "/photos/llewellynphilabuster.jpg",
+        // 10 scores
+        "scores": [
+        2,
+        5,
+        4,
+        2,
+        3,
+        4,
+        5,
+        2,
+        4,
+        3, ]
+},
+    // friend seven
+    {
+        "name": "Philadelphia Washington",
+        "photo": "/photos/leroy-crawford.jpg",
+        // 10 scores
+        "scores": [
+        2,
+        5,
+        4,
+        2,
+        3,
+        4,
+        5,
+        2,
+        4,
+        3, ]
+},
+    // friend eight
+    {
+        "name": "Alberto Gonzales-Giles",
+        "photo": "/photos/leroy-crawford.jpg",
+        // 10 scores
+        "scores": [
+        2,
+        5,
+        4,
+        2,
+        3,
+        4,
+        5,
+        2,
+        4,
+        3, ]
+},
+    // friend nine
+    {
+        "name": "Ingrid-Anne Chrysanthamum",
+        "photo": "/photos/leroy-crawford.jpg",
+        // 10 scores
+        "scores": [
+        2,
+        5,
+        4,
+        2,
+        3,
+        4,
+        5,
+        2,
+        4,
+        3, ]
+},
+    // friend ten
+    {
+        "name": "Mildred James Lansberry",
+        "photo": "/photos/leroy-crawford.jpg",
+        // 10 scores
+        "scores": [
+        2,
+        5,
+        4,
+        2,
+        3,
+        4,
+        5,
+        2,
+        4,
+        3, ]
+}
+
+];
+
+
+
+
+module.exports = friendArr;
